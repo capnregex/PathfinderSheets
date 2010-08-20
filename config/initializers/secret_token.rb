0445ca8f54e83c5123069a56b2532260c35cf88b
Robert Ferney <capnregex@gmail.com>
2010-08-19 23:45:16 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PathfinderSheets::Application.config.secret_token = '9a2b81aa3ae1f6ad2b7df738e7dfe89766fa6968a5db125be45154eb5a6f8b41c03657def1a242cfe75445c714e3de8ab8850f1dd99aa9dd62e1786b57f7e307'
